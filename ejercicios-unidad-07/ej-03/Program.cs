f6010c080a18461283039f9e82cdeaf54318acc7
Facundo Sosa <sosafacundo.94@gmail.com>
2026-02-21 12:54:54 -0300

    Cuso C#

--- ejercicios-unidad-07/ej-03/Program.cs
+++ ejercicios-unidad-07/ej-03/Program.cs
@@ -10,8 +10,13 @@ class Program
         Console.WriteLine("escribe un texto de 10 caracteres");
         for (int i=0; i<10; i++)
         {
-            letra = Convert.ToChar(Console.ReadLine());
+            Console.Write("Ingrese el carácter {0}: ", i + 1);
+            letra = char.Parse(Console.ReadLine());
             letras[i] = letra;
+        }
+        for (int i=0; i<10; i++)
+        {
+            Console.WriteLine("El carácter {0} es: {1}", i + 1, letras[i]);
         }
     }
 }
