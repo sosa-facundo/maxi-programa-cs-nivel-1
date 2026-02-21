2e1f9b79b94e2f48056de2fb2f69284222372566
Facundo Sosa <sosafacundo.94@gmail.com>
2026-02-20 23:15:36 -0300

    Curso C#

--- ejercicios-unidad-07/ej-03/Program.cs
+++ ejercicios-unidad-07/ej-03/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        char letra;
+        char letra2;
+        char[] letras = new char[10];
+        Console.WriteLine("escribe un texto de 10 caracteres");
     }
 }
