1cfd79fcd2ed0d5c01f2c15d317d169ae7d67e3c
Facundo Sosa <sosafacundo.94@gmail.com>
2026-02-24 23:53:31 -0300

    Curso C#

---
+++ ejercicios-unidad-08/ej-02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace ej_02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProcesarNumeros();
+    }
+    static int PedirUnNumero()
+    {
+        Console.WriteLine("Ingrese un numero: ");
+        int num1 = int.Parse(Console.ReadLine());
+        return num1;
+        ///EsPar(int num1);
+    }
+    static bool EsPar(int num1)
+    {
+        if (num1 % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    static void ProcesarNumeros()
+    {
+        for(int x=0; x<20; x++)
+        {
+            int num1 = PedirUnNumero();
+            string impresion = "¿El numero es par?  " + EsPar(num1);
+            Console.WriteLine(impresion);
+        }
+    }
+}
+
