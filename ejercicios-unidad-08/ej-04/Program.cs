95f7233e3d547db6e7632cea5259e502934ad541
Facundo Sosa <sosafacundo.94@gmail.com>
2026-02-25 00:52:43 -0300

    Curso C#

---
+++ ejercicios-unidad-08/ej-04/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ej_04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char letraRef=' ';
+        int numero;
+        Console.WriteLine("Ingrese un numero");
+        numero = int.Parse(Console.ReadLine());
+        PositivoNegativoCero(numero, ref letraRef);
+
+        Console.WriteLine(letraRef);
+    }
+    static void PositivoNegativoCero(int numero, ref char letraRef)
+    {
+        if (numero>0)
+        {
+            letraRef ='a';
+        }
+        if (numero<0)
+        {
+            letraRef ='b';
+        }
+        if (numero==0)
+        {
+            letraRef ='c';
+        }
+    }
+}
